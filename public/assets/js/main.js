030e1bf5a393bec0f25f06963e2b57fb95c8b031
kathimansilla <mansillakatherina@gmail.com>
2023-06-10 04:53:18 +0200

    build basic structure for HTML, SCSS

---
+++ public/assets/js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+/*El ejercicio consiste en desarrollar una aplicación web que contiene un listado de personajes de Disney todo el planeta, que nos permite marcar y desmarcar los personajes como favoritos y guardarlos en localStorage.*/
+//# sourceMappingURL=main.js.map
